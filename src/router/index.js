540ae1514bc575de6dcc1edb914cf018017ce32f
Eddylib <libaoyuxd@gmail.com>
2017-01-03 13:02:10 +0800

    vue slide ok

---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+import HomeView from '../views/Home';
+import LoginView from '../views/Login';
+import RegisterView from '../views/Register';
+import UserView from '../views/User';
+import BorrowRecordsView from '../views/Borrows';
+import BookManagementView from '../views/admin/BookManagement';
+import BorrowManagementView from '../views/admin/BorrowManagement';
+import UserManagementView from '../views/admin/UserManagement';
+
+Vue.use(Router);
+
+var router = new Router({
+  mode: 'history',
+  scrollBehavior: () => ({y: 0}),
+  routes: [
+    { path: '/index', component: RegisterView },
+    { path: '/user/login', component: LoginView },
+    { path: '/user/register', component: RegisterView },
+    { path: '/user', component: UserView },
+    { path: '/user/borrows', component: BorrowRecordsView },
+    { path: '/admin/books', component: BookManagementView },
+    { path: '/admin/borrows', component: BorrowManagementView },
+    { path: '/admin/users', component: UserManagementView },
+    { path: '*', redirect: '/index' }
+  ]
+});
+
+export default router;
